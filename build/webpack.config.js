902a95c56fec8733e21707daae94bba54f20d787
chenliwen <772192993@qq.com>
2018-07-16 23:47:52 +0800

    xml data load,output

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,9 +1,12 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    app: './src/index.js',
+    print: './src/print.js'
+  },
   output: {
-    filename: 'bundle.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, '../dist')
   },
   module: {
@@ -25,6 +28,18 @@ module.exports = {
         test: /\.(woff|woff2|eot|ttf|otf)$/,
         use: [
           'file-loader'
+        ]
+      },
+      {
+        test: /\.(csv|tsv)$/,
+        use: [
+            'csv-loader'
+        ]
+      },
+      {
+        test: /\.xml$/,
+        use: [
+            'xml-loader'
         ]
       }
     ]
--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import _ from 'lodash';
 import './style.css';
 import Icon from './icon.png';
+import Data from './data.xml';
+import printMe from './print.js'
 
 function component() {
   var element = document.createElement('div');
+  var btn = document.createElement('button');
 
   // Lodash（目前通过import依赖库引入）
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
@@ -14,7 +17,13 @@ function component() {
 
   element.appendChild(myIcon);
 
-  return element;
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+  element.appendChild(btn);
+
+  console.log(Data);
+
+    return element;
 }
 
 document.body.appendChild(component());
